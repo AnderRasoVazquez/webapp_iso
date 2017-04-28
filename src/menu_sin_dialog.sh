6c198188ddd4707240f0e91615a8c864d6dcc20a
Ander Raso Vázquez <anderraso@gmail.com>
2017-04-28 20:37:04 +0200

    cambiada la forma de definir las funciones por una mas corta, ahora usa las formas de estilo de '{}' de java

--- src/menu_sin_dialog.sh
+++ src/menu_sin_dialog.sh
@@ -1,9 +1,8 @@
 ##Instalación y Mantenimiento de una Aplicación Web
 #Importar funciones de otros ficheros
 
-function instalarApache() # función 1
+instalarApache() { # función 1
 # Comunicar si el paquete correspondiente ya está instalado y sino instalarlo.
-{
     estado=`sudo aptitude show apache2 | grep "Estado:"`
     echo $estado | grep "no"
     if [ $? = 0 ]
@@ -16,7 +15,7 @@ function instalarApache() # función 1
 }
 #. ./instalarapache.sh
 
-function configurarApache() # función 2
+configurarApache() { # función 2
 # 2.1 Asignar vuestro correo electrónico, como correo del adminastrador del
 # servicio web, para que en caso de cualquier incidencia se le escribir un
 # correo a esa dirección.
@@ -24,87 +23,75 @@ function configurarApache() # función 2
 # /var/www/html.
 # 2.3 El servidor se encuentra por defecto escuchando las peticiones por el
 # puerto 80, se cambiará para que escuche por el puerto 8080.
-{
     echo -e "sin implementar\n"
 }
 
-function probarApache() # función 3
+probarApache() { # función 3
 # 3.1 Poner en marcha el servicio web.
 # 3.2 Testear si el servicio apache2 está en marchar con el comando
 # "netstat -l" (buscar el servicio www).
 # 3.3 Visualizar mediante el navegador en la dirección index.html que aparezca
 # el mensaje: “It works”.
-{
     echo -e "sin implementar\n"
 }
 
-function instalarModuloPHP() # función 4
+instalarModuloPHP() { # función 4
 # Instalar todos los paquetes php.
-{
     echo -e "sin implementar\n"
 }
 
-function configurarModuloPHP() # función 5
+configurarModuloPHP() { # función 5
 # Instalar el paquete de PHPX que permite trabajar con MySQL.
-{
     echo -e "sin implementar\n"
 }
 
-function probarModuloPHP() # función 6
+probarModuloPHP() { # función 6
 # 6.1 Crear el fichero “test.php” en /var/www/ con el siguiente código:
 # ( <?php phpinfo(); ?>)
 # 6.2 Abrir test.php con un navegador web. ¿Qué permisos son necesarios para
 # que podamos verlo todo el mundo?
-{
     echo -e "sin implementar\n"
 }
 
-function instalarModuloMysql() # función 7
+instalarModuloMysql() { # función 7
 # Instalar el paquete Mysql de servidor y cliente.
-{
     echo -e "sin implementar\n"
 }
 
-function configurarModuloMysql() # función 8
+configurarModuloMysql() { # función 8
 # Al usuario administrador de Mysql (“root”) asignarle la contraseña “euiti”.
-{
     echo -e "sin implementar\n"
 }
 
-function probarModuloMysql() # función 9
+probarModuloMysql() { # función 9
 # Testear el servicio mysql "netstat -l" y ponerlo en marcha en caso de estar
 # detenido.
-{
     echo -e "sin implementar\n"
 }
 
-function instalarAplicacion() # función 10
+instalarAplicacion() { # función 10
 # Ejecutar el fichero “web.sql” para crear las tablas y la base datos.
 # Copiar el fichero “consultaprofesores.php” en /var/www/, asignar permisos
 # y comprobar que aparece la información de los profesores en la dirección
 # consultaprofesores.php.
-{
     echo -e "sin implementar\n"
 }
 
-function backupAplicacion() # función 11
+backupAplicacion() { # función 11
 # Crear el fichero comprimido web.tar.gz con los ficheros de “/var/www/”
 # y los datos de la base de datos “web”.
-{
     echo -e "sin implementar\n"
 }
 
-function restaurarAplicacion() #función 12
+restaurarAplicacion() { #función 12
 # Restaurar la base de datos y los ficheros de “/var/www/” desde el fichero
 # web.tar.gz. Aviso: para el backup y restauración de la base de datos,
 # utilizar mysqldump.
-{
     echo -e "sin implementar\n"
 }
 
-function salir() # función 13
+salir() { # función 13
 # Aperece un mensaje de despedida y los nombres de los miembros del grupo.
-{
     read -p "¿Quieres salir del programa?(S/n) " respuesta
     if [ $respuesta != "S" ]
     then
@@ -121,9 +108,10 @@ function salir() # función 13
 
 }
 
-function mostrarOpciones() {
+mostrarOpciones() {
     #Mostrar menu
     echo
+    echo -e "0) Mostar menu"
     echo -e "0) Mostar menu"
     echo -e "1) Instalación de Apache"
     echo -e "2) Configurar el servicio web Apache"
