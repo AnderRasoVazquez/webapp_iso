7d1b5917f1394d4e097000441bb8be4e7af3e612
Ander Raso Vázquez <anderraso@gmail.com>
2017-04-29 17:27:52 +0200

    corregidas las definiciones de funciones, estaban incorrectas y daba error

--- src/funciones/menu_con_dialog/terminar.sh
+++ src/funciones/menu_con_dialog/terminar.sh
@@ -1,3 +1,3 @@
-function terminar(){
-dialog --msgbox "Has elegido la opción de salir, el programa ha terminado\n" 5 50	#Fin del programa
+terminar() {
+    dialog --msgbox "Has elegido la opción de salir, el programa ha terminado\n" 5 50	#Fin del programa
 }
