a56a29c0f68c05f17cf0f0b2d824cb5ba0a5678a
Guillermo19 <gherreramelara@troy.edu>
2017-04-30 22:45:12 +0200

    Retocadala funcion instalarPHP

--- src/funciones/menu_sin_dialog/php/instalarPHP.sh
+++ src/funciones/menu_sin_dialog/php/instalarPHP.sh
@@ -1,13 +1,13 @@
-function instalarPHP()
-{
-	estado=`sudo aptitude show php | grep "State:"`
-	echo $estado | grep "not"
+function instalarPHP() #funcion4
+{ #Instala PHP con el modulo de Apache2
+	estado=`sudo aptitude show php | grep "Estado:"`
+	echo $estado | grep "no"
 	if test $? = 0
 	then 
-	    echo "Installando PHP..."
+	    echo "Installando PHP...\n"
 	    sudo apt install php libapache2-mod-php
 	else
-	  echo "PHP ya estaba instalado"
+	  echo "PHP ya estaba instalado\n"
 	fi 
 }
 #. ./instalarPHP_sin_dialogo.sh
