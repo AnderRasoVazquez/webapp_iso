1333b980621f14945fff42d6f62d6e9db1b14f7d
Ander Raso Vázquez <anderraso@gmail.com>
2017-05-03 19:38:47 +0200

    actualizada la restauración para que se adapte a la nueva configuracion de apache

--- src/restaurar_apache.sh
+++ src/restaurar_apache.sh
@@ -16,6 +16,8 @@ restaurarConfiguracion(){
     sudo sed -i "s/^Listen [0-9]*/Listen ${PORT}/" $PORTS_FILE
     sudo sed -i "s/ServerAdmin .*/ServerAdmin ${EMAIL}/" $CONF_SITE
     sudo sed -i "s/DocumentRoot .*/DocumentRoot ${CARPETA_RAIZ}/" $CONF_SITE
+    sudo sed -i 's/<VirtualHost \*:[0-9]*>/<VirtualHost *:80>/' $CONF_SITE
+    sudo rm /var/www/index.html
 }
 
 reiniciarApache(){
