0a4cc90cade8df4b03327cddf244fc067fcf3c4f
DavidPerezGomez <Davidperezgo@gmail.com>
2017-05-05 12:27:24 +0200

    Cambiado a modo password.

--- src/funciones/menu_sin_dialog/app/backupApp.sh
+++ src/funciones/menu_sin_dialog/app/backupApp.sh
@@ -7,7 +7,7 @@ WEB_SQL="/tmp/web_backup.sql"
 
 backupApp() { # función 11
     cd $ORIGEN
-    read -p "Introduce la contraseña de mysql: " pass
+    read -s -p "Introduce la contraseña de mysql: " pass
     mysqldump -u root -p$pass web > $WEB_SQL &> /dev/null
     if [ $? == 0 ]
     then
