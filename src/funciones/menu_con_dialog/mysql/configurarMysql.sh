12490eacf763e3044db14de16cf1f3f6f69bddb1
Ander Raso Vázquez <anderraso@gmail.com>
2017-05-05 12:23:07 +0200

    cambiado inputbox por password box

--- src/funciones/menu_con_dialog/mysql/configurarMysql.sh
+++ src/funciones/menu_con_dialog/mysql/configurarMysql.sh
@@ -8,7 +8,8 @@ SALIDA='/tmp/salida.txt'
 
 configurarMysql() { # función 8
 # Al usuario administrador de Mysql (“root”) asignarle la contraseña “euiti”.
-    dialog --inputbox "Introduce la contraseña actual de MySQL:" 8 40 2> $SALIDA
+    dialog --insecure --passwordbox "Introduce la contraseña actual de MySQL:" 8 40 2> $SALIDA
+    # dialog --inputbox "Introduce la contraseña actual de MySQL:" 8 40 2> $SALIDA
     pass=`less $SALIDA`
     rm $SALIDA
     # Se intenta entrar a mysql con la contraseña dada y cambiarla a la nueva.
